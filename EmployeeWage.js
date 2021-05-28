220c252f23e4c831625b41aeca7a2b65179c1853
vundelayamini <vundelayamini@gmail.com>
2021-05-28 09:03:35 +0530

    [yamini]Add: Write the function to get Working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 //UC1 Ability to check Employees is present or absent.
 //Constants
-{
+
     console.log("Welcome To Employee Wage Problem");
     const IS_ABSENT = 0;
     //Using random function to identify employee is present or not
@@ -14,6 +14,7 @@
         console.log("Employee is present");
     }
     //UC2 Ability to Calculate Daily Employee Wage 
+    console.log("UC2:Calculta Employee Daily wage");
     //Constants
     const IS_FULL_TIME=1;
     const IS_PART_TIME=2;
@@ -34,8 +35,26 @@
     }
     let empwage=empHrs*WAGE_PER_HR;
     console.log("Daily Emp Wage:"+empwage);
-}
-   
+
+
+    //UC3-Write a function to get working hours
+    //Constants
+    function getWorkingHrs(empCheck)
+    {
+        switch(empCheck)
+       {
+            case IS_PART_TIME:
+                return PART_TIME_HRS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HRs;
+            default:
+               return 0;
+       }
+
+    }
+    let empHr=getWorkingHrs(empCheck);
+    let empwages=empHr*WAGE_PER_HR;
+    console.log("UC3 implimented functions:Dailywage is:" +empwages);
 
 
  
